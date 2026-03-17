d88ba051c1ae88a2ccc5933081ad7b42b37f290c
eddymadu14 <edwardmaduneme.com>
2026-03-17 11:10:52 +0100

    book update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import axios from "axios";
-import registerBookMailerRoutes from "./book";
+import registerBookMailerRoutes from "./book.js";
 
 dotenv.config();
 
